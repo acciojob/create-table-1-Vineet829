156962a905a9735e196d2f9d00c93350764a4262
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-28 20:48:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 function insert_Row() {
     let table = document.querySelector("#sampleTable");
-    let temp = table.children;
-    let arr = Array.from(temp); 
-
     let newRow = `<tr><td>New Cell1</td><td>New Cell2</td></tr>`;
     
-    table.innerHTML = newRow + table.innerHTML;
-
-    
-    for (let i of arr) {
-        table.appendChild(i);
-    }
+  
+    table.insertAdjacentHTML('afterbegin', newRow);
 }
-
