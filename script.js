83c7e026f6d90254f122fdba76df11e1ff488970
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-28 20:56:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function insert_Row() {
     let table = document.querySelector("#sampleTable");
+    let tbody = table.querySelector("tbody:first-of-type");
     let newRow = `<tr><td>New Cell1</td><td>New Cell2</td></tr>`;
-    
-  
-    table.insertAdjacentHTML('afterbegin', newRow);
+    tbody.insertAdjacentHTML('afterbegin', newRow);
+	
 }
+
